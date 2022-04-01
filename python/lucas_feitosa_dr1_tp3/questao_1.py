6fc50a15f64a9fb6a66f751535c7bc1a4d5a783d
Lucas Feitosa <lucasfeitosapr@gmail.com>
2022-03-31 23:41:15 -0300

    Refatorando questão um: criando função para retornar o resultado

--- python/lucas_feitosa_dr1_tp3/questao_1.py
+++ python/lucas_feitosa_dr1_tp3/questao_1.py
@@ -6,33 +6,37 @@ def divisao_conta():
     total_consumo: float = 0.0
     percentual_servico: float = 0.0
     resultado = 0
-    error_message = 'Valor Inválido'
-    success_message = 'Dividindo a conta por {0} pessoa(s), cada pessoa deverá pagar R$ {1}'
+    mensagem_erro = 'Valor Inválido'
+    mensagem_sucesso = 'Dividindo a conta por {0} pessoa(s), cada pessoa deverá pagar R$ {1}'
 
     print('Insira o total de pessoas: ')
     total_pessoas = int(input())
 
     if total_pessoas <= 0:
-        print(error_message)
+        print(mensagem_erro)
         return
 
     print('Insira o percentual de serviço entre 0 e 100: ')
     percentual_servico = float(input())
 
     if percentual_servico < 0 or percentual_servico > 100:
-        print(error_message)
+        print(mensagem_erro)
         return
 
     print('Insira o total do consumo: R$ ')
     total_consumo = float(input())
 
     if total_consumo < 0:
-        print(error_message)
+        print(mensagem_erro)
         return
 
-    resultado = (total_consumo + percentual_servico)/total_pessoas
-    resultado = str('%.2f' % resultado).replace('.',',')
+    resultado = divide_conta(total_consumo, percentual_servico, total_pessoas)
+    resultado = str('%.2f' % resultado).replace('.', ',')
 
-    print(success_message.format(total_pessoas, resultado))
+    print(mensagem_sucesso.format(total_pessoas, resultado))
+
+
+def divide_conta(total_consumo, percentual_servico, total_pessoas):
+    return (total_consumo + percentual_servico) / total_pessoas
 
 divisao_conta()
